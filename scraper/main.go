224fdb605d11a8a75d3b256630d7d3095e9c43f4
E <e@pastel.codes>
2024-05-15 15:00:34 +0100

    WIP: Start selecting elements to check for

--- scraper/main.go
+++ scraper/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/base64"
+	"errors"
 	"log"
 	"os"
 
@@ -14,14 +15,53 @@ import (
 )
 
 type MyEvent struct {
-	URL   string `json:"URL"`
-	XPATH string `json:"XPATH"`
+	URL                 string `json:"url"`
+	PreviousPrice       string `json:"previous_price"`
+	PreviousStockStatus string `json:"previous_stock_status"`
+	PriceXPATH          string `json:"price_xpath"`
+	InStockString       string `json:"in_stock_string"`
+	OutStockString      string `json:"out_stock_string"`
 }
 
 type MyResponse struct {
-	Content string `json:"content"`
-	Hash    string `json:"hash"`
+	HTML    string `json:"html"`
+	Price   string `json:"price"`
+	InStock bool   `json:"in_stock"`
 	Image   string `json:"image"`
+	Error   string `json:"error"`
+}
+
+func getStock(page *rod.Page, inStockString string, outStockString string) (*bool, error) {
+	inStockElement := page.MustElementR("button", "/"+inStockString+"/i")
+	outOfStockElement := page.MustElementR("button", "/"+outStockString+"/i")
+
+	stockStatus := new(bool)
+
+	if inStockElement != nil && outOfStockElement != nil {
+		return stockStatus, errors.New("stock: both in and out of stock")
+	} else if inStockElement == nil && outOfStockElement == nil {
+		return stockStatus, errors.New("stock: neither in and out of stock")
+	}
+
+	if inStockElement != nil {
+		*stockStatus = true
+	} else if outOfStockElement != nil {
+		*stockStatus = false
+	} else {
+		return stockStatus, errors.New("stock: uncaught error")
+	}
+
+	return stockStatus, nil
+}
+
+func takeScreenShot(page *rod.Page) (string, error) {
+	bytes, err := page.Screenshot(false, &proto.PageCaptureScreenshot{})
+
+	if err != nil {
+		return "", err
+	}
+
+	return "data:image/png;base64," + base64.StdEncoding.EncodeToString(bytes), nil
 }
 
 func scrape(ctx context.Context, event *MyEvent) (*MyResponse, error) {
@@ -29,14 +69,29 @@ func scrape(ctx context.Context, event *MyEvent) (*MyResponse, error) {
 	u := launcher.New().Bin(CHROME_PATH).MustLaunch()
 	page := rod.New().ControlURL(u).MustConnect().MustPage(event.URL)
 	page.MustWaitStable()
-	bytes, err := page.Screenshot(false, &proto.PageCaptureScreenshot{})
+
+	// check price xpath and get string
+	// check stock string and get string
+
+	// some sort of check here so we don't do all this stuff every time if the page hasn't changed
+
+	stockStatus, err := getStock(page, event.InStockString, event.OutStockString)
 
 	if err != nil {
 		log.Fatal(err)
+		return &MyResponse{}, err
 	}
 
-	base64Encoding := "data:image/png;base64," + base64.StdEncoding.EncodeToString(bytes)
-	return &MyResponse{Content: "Test", Hash: "Test", Image: base64Encoding}, nil
+	base64EncodedImage, err := takeScreenShot(page)
+
+	if err != nil {
+		log.Fatal(err)
+		return &MyResponse{}, err
+	}
+
+	return &MyResponse{HTML: "Test", Price: "Test", Image: base64EncodedImage, InStock: *stockStatus}, nil
+
+	// if check didnt pass, send error
 }
 
 func main() {
